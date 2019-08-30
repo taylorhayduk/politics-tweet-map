c847f22e3370df6ad6067c855332df3d25856fa9
Taylor Hayduk <taylorhayduk@gmail.com>
2019-08-30 07:52:23 -0400

    Add Google Civic Information API

---
+++ server/CivicInformation/api.js
@@ -0,0 +1,29 @@
+const axios = require('axios');
+const baseURL = 'https://www.googleapis.com/civicinfo/v2';
+const key = process.env.CIVIC_INFORMATION_API_KEY;
+
+const api = axios.create({
+  baseURL,
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  params: { key }
+});
+
+class CivicInformationApi {
+  static representativeInfoByAddress(address) {
+    return api
+      .get(`/representatives`, {
+        params: {
+          address
+        }
+      })
+      .then(({ data }) => data);
+  }
+
+  static representativeInfoByDivision(ocdId) {
+    return api.get(`/representatives/${ocdId}`).then(({ data }) => data);
+  }
+}
+
+module.exports = CivicInformationApi;
