5aab400d82ebf8481d5bf505d0b818839793739f
Taylor Hayduk <taylorhayduk@gmail.com>
2019-09-02 16:44:53 -0400

    Graphql logo and link

--- pages/index.js
+++ pages/index.js
@@ -20,6 +20,13 @@ class App extends Component {
     return (
       <ApolloProvider client={client}>
         <div>
+          <a
+            href={
+              '/graphql?query=%7B%0A%20%20officials%28address%3A%20%22Georgia%2C%20USA%22%29%20%7B%0A%20%20%20%20name%0A%20%20%20%20party%0A%20%20%20%20photoUrl%0A%20%20%20%20address%20%7B%0A%20%20%20%20%20%20city%0A%20%20%20%20%20%20state%0A%20%20%20%20%7D%0A%20%20%20%20channels%20%7B%0A%20%20%20%20%20%20type%0A%20%20%20%20%20%20id%0A%20%20%20%20%7D%0A%20%20%20%20tweets%28limit%3A3%29%20%7B%0A%20%20%20%20%20%20text%0A%20%20%20%20%7D%0A%20%20%7D%0A%7D%0A'
+            }
+          >
+            <img className="spinLogo" src="/static/GraphQL_Logo.png" />
+          </a>
           <div style={{ display: 'flex', justifyContent: 'center' }}>
             <Map handleStateClick={this.handleStateClick} />
           </div>
@@ -27,6 +34,23 @@ class App extends Component {
           <style jsx global>{`
             body {
               font-family: Helvetica Neue, Helvetica, Arial, Utkal, sans-serif;
+            }
+            .spinLogo {
+              animation-name: spin;
+              animation-duration: 5000ms;
+              animation-iteration-count: infinite;
+              animation-timing-function: linear;
+              position: fixed;
+              top: 10px;
+              right: 10px;
+            }
+            @keyframes spin {
+              from {
+                transform: rotate(0deg);
+              }
+              to {
+                transform: rotate(360deg);
+              }
             }
           `}</style>
         </div>
